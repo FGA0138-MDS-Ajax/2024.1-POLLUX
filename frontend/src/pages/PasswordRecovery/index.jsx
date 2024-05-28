cc13044ead20a1012c98d8b587eaecbe3852399c
femathrl <84880995+femathrl@users.noreply.github.com>
2024-05-28 00:04:25 -0300

    Tela Recuperar Senha

--- frontend/src/pages/PasswordRecovery/index.jsx
+++ frontend/src/pages/PasswordRecovery/index.jsx
@@ -2,14 +2,43 @@ import React, { useState } from 'react';
 import './PasswordRecovery.css';
 import { Link } from 'react-router-dom';
 
-function passwordRecovery() {
-    return (
-        <>
-        <p>
-                aaaaaa
-        </p>
-        </>
-            )
-            }
+function PasswordRecovery() {
 
-            export default passwordRecovery;
+const [email, setEmail] = useState('');
+
+const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+}
+
+const handleSubmit = (event) =>{
+    event.preventDefault();
+    console.log("email:", email)
+
+    setEmail('');
+
+    alert("email : "+email);
+}
+
+    return (
+        <body id="paginaPasswordRecovery">
+            <div className=' divC'>
+                <Link to= '/Login'>
+                    <img className='img-setaRecovery' src='/seta.svg' alt ='seta'/>
+                </Link>
+                <span className='title'> Esqueceu a senha?</span>
+                <span className='sub-title'> Coloque seu e-mail</span>
+                <form onSubmit={handleSubmit}>
+                    <input
+                    type = "text"
+                    placeholder='E-mail'
+                    value ={email}
+                    onChange = {handleEmailChange}
+                    />
+                    <button type="submit"> Enviar</button>
+                </form>
+            </div>
+        </body>
+);
+}
+
+export default PasswordRecovery;
