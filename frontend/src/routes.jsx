658fa5ede1e50092f0639c3a1e48e93e0888508a
femathrl <84880995+femathrl@users.noreply.github.com>
2024-05-28 00:05:13 -0300

    Tela Recuperar Senha

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Divulgation from './pages/Divulgation'
 import Login from './pages/Login'
+import PasswordRecovery from './pages/PasswordRecovery'
 import CreateAccout from './pages/CreateAccount'
 import './App.css'
 
@@ -11,6 +12,7 @@ function AppRoutes() {
                 <Route path='/' element={<Divulgation />}></Route>
                 <Route path='/login' element={<Login />}></Route>
                 <Route path='/createAccount' element={<CreateAccout />}/>
+                <Route path='/passwordRecovery' element={   <PasswordRecovery />}/>
             </Routes>
         </BrowserRouter>
     )
