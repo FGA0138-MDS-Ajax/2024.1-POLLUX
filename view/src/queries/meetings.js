df73a8a2818bf4ffa99924a60149fa0d3bd2f216
jazzer0 <joaoricardo.silver@gmail.com>
2024-06-18 23:00:24 -0300

    feat: endpoints Reuniões

---
+++ view/src/queries/meetings.js
@@ -0,0 +1,52 @@
+import server from "../config/axiosInstance";
+import endpoints from "../constants/endpoints";
+
+export const getMeetings = async () => {
+  try {
+    const meeting = await server.get(endpoints.meeting.base);
+    return meeting;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const getSingleMeeting = async (meetingId) => {
+  try {
+    const meeting = await server.get(endpoints.meeting.single(meetingId));
+    return meeting;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const createMeeting = async (data) => {
+  try {
+    const meeting = await server.post(endpoints.meeting.base, data);
+    return meeting;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const editMeeting = async (meetingId, data) => {
+  try {
+    const meeting = await server.put(endpoints.meeting.single(meetingId), data);
+    return meeting;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const deleteMeeting = async (meetingId) => {
+  try {
+    const meeting = await server.delete(endpoints.meeting.single(meetingId));
+    return meeting;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
