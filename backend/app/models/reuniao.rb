97da8559c1d15e0fe504dcee973a53337663280c
Mauriciofearauj <mauriciofearatrabalho@gmail.com>
2024-06-29 23:29:29 -0300

    teste unitarios.2

--- backend/app/models/reuniao.rb
+++ backend/app/models/reuniao.rb
@@ -1,2 +1,8 @@
 class Reuniao < ApplicationRecord
+    belongs_to :user
+    has_many :reunioes_links, dependent: :destroy
+    has_many :reunioes_usuarios, dependent: :destroy
+    accepts_nested_attributes_for :reunioes_links
+    accepts_nested_attributes_for :reunioes_usuarios
+    validates :user, presence: true
 end
